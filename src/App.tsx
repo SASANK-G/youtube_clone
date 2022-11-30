871b1386326f4c961cbdf261330bc1ee561b55b4
sasank <gsbnsrsasank@gmail.com>
2022-11-30 12:31:18 +0530

    React router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {BrowserRouter,Routes, Route} from "react-router-dom"
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Watch from './pages/Watch'
 
 function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>Hehehe</h1>
+    <BrowserRouter>
+    <Routes>
+      
+      <Route path='/search'element={<Search/>}/>
+      <Route path='/watch/:id'element={<Watch/>}/>
+      <Route path='/'element={<Home/>}/>
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
 
